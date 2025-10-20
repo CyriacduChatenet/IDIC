1b1e95ec4b7d2a9d00b58226b3b34d82a653deb9
CyriacduChatenet <cyriacduchatenet@icloud.com>
2025-10-20 23:39:19 +0200

    feat(api): add teams endpoints

--- api/src/team/team.module.ts
+++ api/src/team/team.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { HttpModule } from '@nestjs/axios';
+
 import { TeamService } from './team.service';
 import { TeamController } from './team.controller';
+import { StrapiService } from '../strapi/strapi.service';
 
 @Module({
+  imports: [HttpModule],
   controllers: [TeamController],
-  providers: [TeamService],
+  providers: [TeamService, StrapiService],
 })
 export class TeamModule {}
--- api/src/team/team.service.ts
+++ api/src/team/team.service.ts
@@ -1,26 +1,83 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+
 import { CreateTeamDto } from './dto/create-team.dto';
 import { UpdateTeamDto } from './dto/update-team.dto';
+import {
+  StrapiApiCreateResponse,
+  StrapiApiDeleteResponse,
+  StrapiApiFindAllResponse,
+  StrapiApiFindOneResponse,
+  StrapiApiUpdateResponse,
+} from '../config/interfaces/strapi-api-response.interface';
+import { Team } from './entities/team.entity';
+import { StrapiService } from '../strapi/strapi.service';
 
 @Injectable()
 export class TeamService {
-  create(createTeamDto: CreateTeamDto) {
-    return 'This action adds a new team';
+  constructor(private readonly strapiService: StrapiService) {}
+
+  create(createTeamDto: CreateTeamDto): Promise<StrapiApiCreateResponse<Team>> {
+    try {
+      return this.strapiService.postData('teams', {
+        data: createTeamDto,
+      });
+    } catch (err) {
+      console.error('Error creating team:', err);
+      throw new ForbiddenException(
+        `You do not have PERMISSION to create a STRAPI_TEAM`,
+      );
+    }
   }
 
-  findAll() {
-    return `This action returns all team`;
+  findAll(): Promise<StrapiApiFindAllResponse<Team>> {
+    try {
+      return this.strapiService.getAllData('teams');
+    } catch (err) {
+      console.error('Error fetching teams:', err);
+      throw new NotFoundException(`No STRAPI_TEAMS found`);
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} team`;
+  findOne(id: string): Promise<StrapiApiFindOneResponse<Team>> {
+    try {
+      return this.strapiService.getDataById(`teams/${id}`);
+    } catch (err) {
+      console.error('Error fetching taem:', err);
+      throw new NotFoundException(`STRAPI_TEAM with ID ${id} not found`);
+    }
   }
 
-  update(id: number, updateTeamDto: UpdateTeamDto) {
-    return `This action updates a #${id} team`;
+  update(
+    id: string,
+    updateTeamDto: UpdateTeamDto,
+  ): Promise<StrapiApiUpdateResponse<Team>> {
+    try {
+      return this.strapiService.updateData(`teams/${id}`, {
+        data: updateTeamDto,
+      });
+    } catch (err) {
+      console.error('Error updating team:', err);
+      throw new ForbiddenException(
+        `You do not have permission to update STRAPI_TEAM with ID ${id} and CRDENTIALS ${JSON.stringify(updateTeamDto)}`,
+      );
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} team`;
+  async remove(id: string): Promise<StrapiApiDeleteResponse<Team>> {
+    try {
+      const team = await this.findOne(id);
+      await this.strapiService.deleteData(`teams/${id}`);
+
+      return { data: team.data };
+    } catch (err) {
+      console.error('Error deleting team:', err);
+      throw new ForbiddenException(
+        `You do not have permission to delete STRAPI_TEAM with ID ${id}`,
+      );
+    }
   }
 }
